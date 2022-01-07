c6606b0e8cdb9047f629251e80a870ab67713ef2
w3ssfs <wesleyfranca001@gmail.com>
2022-01-07 14:41:38 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import instaloader
+
+# Carregar a Lib e faz Login com a conta desejada #
+
+L = instaloader.Instaloader()
+L.login('USER', 'SENHA')
+
+# Carrega todos os posts do perfil escolhido #
+posts = instaloader.Profile.from_username(L.context, 'PERFIL ESCOLHIDO').get_posts()
+
+# Período que deseja baixar os Posts #
+SINCE = datetime(2021,1,16)
+UNTIL = datetime(2022,1,18)
+
+# Percorre os posts e baíxa apenas os que estão dentro do período desejado #
+for post in posts:
+    if (post.date >= SINCE) and (post.date <= UNTIL):
+        print(post.date)
+        L.download_post(post, 'insta-posts-downloads')
